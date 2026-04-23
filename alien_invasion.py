d1f446d5de0cc6c99acb903da01662ac5b0aef68
Kennett <kaguilarzaldana@student.cscc.edu>
2026-04-22 23:20:33 -0400

    P3 - Set up button to start game and make mouse disapper, made sure no keydown events are captured until button is pressed.   Fixed button fill rect error

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,6 +17,7 @@ from arsenal import Arsenal
 # from alien import Alien
 from alien_fleet import AlienFleet
 from time import sleep
+from button import Button
 
 class AlienInvasion:
     """Manges game resources, settings, and overall game behavior.
@@ -53,7 +54,9 @@ class AlienInvasion:
         self.ship = Ship(self, Arsenal(self))
         self.alien_fleet = AlienFleet(self)
         self.alien_fleet.create_fleet()
-        self.game_active = True
+
+        self.play_button = Button(self, 'Play')
+        self.game_active = False
 
     def run_game(self):
         """Start and maintain the main game loop.
@@ -110,6 +113,14 @@ class AlienInvasion:
         self.alien_fleet.fleet.empty()
         self.alien_fleet.create_fleet()
 
+    def restart_game(self):
+        # setting up dynamic Settings
+        # reset Game stats
+        # update HUD scores
+        self,self._reset_level()
+        self.ship._center_ship()
+        self.game_active = True
+        pygame.mouse.set_visible(False)
 
     def _update_screen(self):
         """Redraw the background, ship, and bullets on each frame.
@@ -117,6 +128,11 @@ class AlienInvasion:
         self.screen.blit(self.bg, (0,0))
         self.ship.draw()
         self.alien_fleet.draw()
+
+        if not self.game_active:
+            self.play_button.draw()
+            pygame.mouse.set_visible(True)
+
         pygame.display.flip()
 
     def _check_events(self):
@@ -127,10 +143,17 @@ class AlienInvasion:
                 self.running = False
                 pygame.quit()
                 sys.exit()
-            elif event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN and self.game_active == True:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self._check_button_clicked()
+
+    def _check_button_clicked(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.play_button.check_clicked(mouse_pos):
+            self.restart_game()
 
     def _check_keyup_events(self, event):
         """Stop ship movement when a movement key is released.
--- button.py
+++ button.py
@@ -24,7 +24,7 @@ class Button:
         self.msg_image_rect.center = self.rect.center
 
     def draw(self):
-        self.screen.fill(self.settings.button_color, self.rect.center)
+        self.screen.fill(self.settings.button_color, self.rect)
         self.screen.blit(self.msg_image, self.msg_image_rect)
 
     def check_clicked(self, mouse_pos):
