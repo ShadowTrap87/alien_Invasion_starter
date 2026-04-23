1a526d8636393fb43c5567ad98be4e9ac66236c4
Kennett <kaguilarzaldana@student.cscc.edu>
2026-04-22 23:37:23 -0400

    P3 - Plugged in difficulty settings and initial dynamic settings for the alien invasion game

--- alien_invasion.py
+++ alien_invasion.py
@@ -91,6 +91,9 @@ class AlienInvasion:
 
         if self.alien_fleet.check_destroyed_status():
             self._reset_level()
+            self.settings.increase_difficulty()
+            # update game stats level
+            # update HUD view
         
 
     def _check_game_status(self):
@@ -115,7 +118,7 @@ class AlienInvasion:
         self.alien_fleet.create_fleet()
 
     def restart_game(self):
-        # setting up dynamic Settings
+        self.settings.initialize_dynamic_settings()
         # reset Game stats
         # update HUD scores
         self,self._reset_level()
@@ -129,6 +132,7 @@ class AlienInvasion:
         self.screen.blit(self.bg, (0,0))
         self.ship.draw()
         self.alien_fleet.draw()
+        # draw HUD
 
         if not self.game_active:
             self.play_button.draw()
